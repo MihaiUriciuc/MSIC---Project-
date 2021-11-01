145d37c9664bf2058303d06a8b929a5d1a25bf8d
MihaiUriciuc <mihaia932@gmail.com>
2021-11-01 21:48:55 +0200

    Date

--- Project-Msic/src/Date.java
+++ Project-Msic/src/Date.java
@@ -1,4 +1,11 @@
 
 public class Date {
+	int apa_rece; /*valoarea consumului de apa rece pe luna curenta */
+	int apa_calda; /*valoarea consumului de apa calda pe luna curenta*/
+	int energ; /*valoarea consumului de curent pe bloc repartizat pe locatar*/
+	int rep; /*fondul de reparatii per locuinta*/
+	int suma; /*intretinerea pe luna curenta*/
+	int sal; /*suma de bani preluata de la fiecare apartament in parte pentru a 
+	           plati serviciile de curatenie si administrare bloc*/
 
 }
