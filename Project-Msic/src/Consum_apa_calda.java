3d213f3a050695e8b8699a466c99daebb58f3a2c
MihaiUriciuc <mihaia932@gmail.com>
2021-11-10 21:23:34 +0200

    Small changes

--- Project-Msic/src/Consum_apa_calda.java
+++ Project-Msic/src/Consum_apa_calda.java
@@ -1,8 +1,8 @@
 
 public class Consum_apa_calda extends Consum_apa_rece{
-	public float apa_calda()
+	public float apa_calda(float apa_r)
 	{
-		float apa_r= (float) (apa_calda*3); /*valoarea consumului de apa rece*/ 
+		apa_r= (float) (apa_calda*3); /*valoarea consumului de apa rece*/ 
 
 		return apa_r;
 		
--- Project-Msic/src/Consum_apa_rece.java
+++ Project-Msic/src/Consum_apa_rece.java
@@ -1,8 +1,8 @@
 
 public class Consum_apa_rece extends Date {
-	public float apa_rece()
+	public float apa_rece(float apa_r)
 	{
-		float apa_r= (float) (apa_calda*1.8); /*valoarea consumului de apa rece*/ 
+		apa_r = (float) (apa_calda*1.8); /*valoarea consumului de apa rece*/ 
 
 		return apa_r;
 		
