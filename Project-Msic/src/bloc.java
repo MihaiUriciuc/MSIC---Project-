9101fab99b0ca97e323ea7044d94bdf2d9f18d62
MihaiUriciuc <mihaia932@gmail.com>
2021-11-15 21:36:02 +0200

    Small change

--- Project-Msic/src/bloc.java
+++ Project-Msic/src/bloc.java
@@ -8,9 +8,12 @@ public class bloc {
 	private int nr_ap;  /*nr de apartamente din bloc*/
 	private int nr_cheltuieli; /*cheltuielile lunare pe bloc*/
 
-	public bloc(int id) {
+	public bloc(int id, int scara, int nr_ap, int nr_cheltuieli) {
 		this.id=id;
-		
+		this.scara=scara;
+		this.nr_ap=nr_ap;
+		this.nr_cheltuieli=nr_cheltuieli;
+
 	}
 	public void setScara(int scara)
 	{
@@ -26,4 +29,24 @@ public class bloc {
 	}
 	
 
+	public int getId(int id)
+	{
+		return id;
+	}
+	
+	public int getscara(int scara)
+	{
+		return scara;
+	}
+
+	public int getnrap(int ap)
+	{
+		return ap;
+	}
+	
+	public int getnr_cheltuieli(int nr_cheltuieli)
+	{
+		return nr_cheltuieli;
+	}
+	
 }
